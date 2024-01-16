a7816f9ccc827b6d48fac4ffb648bbab0ae769fe
Marinho <marinhoalexandreneves@gmail.com>
2024-01-15 23:27:39 -0300

    Aula de particularidades do js

---
+++ exercícios/3_particularidades_js/js/scripts.js
@@ -0,0 +1,22 @@
+console.log("primeiro");
+
+console.log("Último");
+
+console.log("Mais um console.log");
+
+var a = 1;
+a = "teste";
+
+console.log(a);
+
+
+//comentário de uma linha
+
+/* 
+Este 
+é
+um 
+comentário
+de múltiplas
+linhas    
+*/
