e185a11f875422def72058b120e3f23c546302bd
Marinho <marinhoalexandreneves@gmail.com>
2024-01-18 23:22:28 -0300

    strings

---
+++ exercícios/6_string/js/scripts.js
@@ -0,0 +1,25 @@
+var nome = "Marinho";
+var sobreNome = "Neves";
+
+var nomeCompleto = nome + " " + sobreNome; //concatenação
+
+console.log(nome);
+
+console.log(typeof nome);
+
+console.log(nomeCompleto);
+
+console.log(typeof "5.292929");
+
+var frase = "Está é uma frase complexa";
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.write('Ele disse:"Olá"');
+
+document.write("Ele disse: 'Olá'");
+
+console.log("Este número: " + "432");
