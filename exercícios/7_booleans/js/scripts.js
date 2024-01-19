9208ddce2190fa1652f5cd2d3cb9ba36ac4f82a0
Marinho <marinhoalexandreneves@gmail.com>
2024-01-19 19:21:22 -0300

    Verdadeiro ou falso do js

---
+++ exercícios/7_booleans/js/scripts.js
@@ -0,0 +1,21 @@
+var verdadeira = true;
+
+console.log(verdadeira);
+console.log(typeof verdadeira);
+
+console.log(typeof true);
+console.log(typeof false);
+
+var falso = false;
+
+console.log(falso);
+console.log(typeof falso);
+
+// nomes reservados 
+
+/*
+    var
+    false
+    true
+    function
+*/
