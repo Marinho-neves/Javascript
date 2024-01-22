df4a45fba9d1984203c24aea8327d4c6429c3ccb
Marinho <marinhoalexandreneves@gmail.com>
2024-01-22 19:59:12 -0300

    Null_underfindned

---
+++ exercícios/8_Null_undefined/js/scripts.js
@@ -0,0 +1,12 @@
+// Hoisting - içamento
+console.log(sobrenome);
+
+var nome = null;
+var sobrenome = "Neves";
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "Marinho";
+
+console.log(nome);
