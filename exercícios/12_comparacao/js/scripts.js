2c277587a5305d69eb1df2269932f3849bb05f04
Marinho <marinhoalexandreneves@gmail.com>
2024-01-26 23:55:11 -0300

    Operadores de comparação em js

---
+++ exercícios/12_comparacao/js/scripts.js
@@ -0,0 +1,27 @@
+var idade = 17;
+var possuicarro = 1;
+
+if(idade >= 18){
+    console.log("Pode tirar a carteira de motorista!");
+}
+/*
+else{
+    console.log("Não pode tirar a carteira!");
+}
+*/
+if(idade <= 17){
+    console.log("Não pode tirar a carteira!");
+}
+if(possuicarro){
+    console.log("O usuário já pode andar de carro");
+}
+
+var nome = "Marinho";
+
+if(nome == "Marinho"){
+    console.log("Meu nome é Marinho");
+}
+
+if(20 > 10 ){
+    console.log("Passou 2");
+}
