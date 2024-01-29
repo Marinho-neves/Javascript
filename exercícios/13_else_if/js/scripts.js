388f701dcd88ecb70bb3502a43236c4a54b1e952
Marinho <marinhoalexandreneves@gmail.com>
2024-01-29 19:37:08 -0300

    Else js

---
+++ exercícios/13_else_if/js/scripts.js
@@ -0,0 +1,25 @@
+var nome = "José";
+
+if (nome == "Pedro"){
+    console.log("O nome dele é Pedro");
+}else if(nome == "Marinho"){
+    console.log("O nome dele é Marinho");
+}else if(nome == "José"){
+    console.log("O nome dele é José");
+}else{
+    console.log("Ele possui outro nome!");
+} 
+
+var idade = 19;
+
+if(idade > 20){
+    console.log("Ele pode entrar na festa!");
+}else if (idade >= 18){
+    console.loh("Ele só pode entrar com autorização");
+}
+
+if(nome == "Marinho"){
+    console.log("teste");
+}else{
+    comsole.log("Testando");
+}
