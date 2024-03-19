7ee76f65c7b5777a3b8574a1a39591b163a6295e
Marinho <marinhoalexandreneves@gmail.com>
2024-03-19 14:30:08 -0300

    estrutura while

---
+++ exercícios/17_repetição/js/scripts.js
@@ -0,0 +1,22 @@
+var x = 0;
+
+
+while(x < 5){
+    console.log("Testando repetição" + x);
+    x++;
+}
+var y = 0;
+
+var arr = ['teste', 'testando', 'a','b'];
+while (y <= 3 ){
+    console.log(arr[y]);
+    y++;
+}
+
+var palavra = "Marinho";
+var i = 0;
+
+while( i <= 7){
+    console.log(palavra[i]);
+    i++;
+}
