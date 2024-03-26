80dedf1bedd7f56a52700f608af8921f1a515246
Marinho <marinhoalexandreneves@gmail.com>
2024-03-26 01:08:55 -0300

    estudo de função

---
+++ exercícios/21_function/js/scripts.js
@@ -0,0 +1,30 @@
+function primeirafuncao(){
+    console.log("Hello world das funções!");
+}
+primeirafuncao();
+
+function dizerNome(nome){
+    console.log("O nome é:" + nome);
+}
+
+dizerNome(" Marinho");
+dizerNome(" Maria");
+dizerNome(" Cristina");
+
+var nomedoBancodeDados = " clientes";
+
+dizerNome(nomedoBancodeDados);
+
+function soma (a, b) {
+   var soma = a + b;
+    return soma;
+}
+var somaUm = soma(2, 5);
+
+console.log(somaUm);
+
+var somaDois = soma (5, 5);
+
+console.log(somaDois);
+
+console.log(soma(4,5));
