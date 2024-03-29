6378571e793500ed9cf51e5c3f5ffee3c14188c0
Marinho <marinhoalexandreneves@gmail.com>
2024-03-29 14:36:33 -0300

    estudo de let e const

---
+++ exercícios/24_let_const_escopo/js/scripts.js
@@ -0,0 +1,31 @@
+let x = 5; //var x = 5;
+
+x = 12
+
+const y = 10;
+
+console.log(x);
+
+console.log('cosnt: ' + y);
+
+if(true){
+
+    let x = 20;
+    
+    console.log(x);
+
+    const y = 50;
+
+    console.log('cosnt if: ' + y);
+}
+console.log(x);
+
+if (20 > 10){
+    const y = 100;
+
+    console.log('cosnt if 3: ' + y);
+}
+
+for (let x = 0; x < 10; x++){
+    console.log(x);
+}
